90cf02e70f7e28eecc7b1cefaf69e99bb66f426d
Will Saults <wsaults@yahoo.com>
2017-06-06 11:01:06 -0500

    Added status.addListenter and suggestions helper functions

--- status/public/bot/bot.js
+++ status/public/bot/bot.js
@@ -13,7 +13,19 @@ status.command({
               type: status.types.TEXT,
               suggestions: helloSuggestions
              }]
- })
+ });
+
+ status.command({
+     name: "apply",
+     title: "Apply for a policy",
+     description: "Options for applying",
+     color: "#0000ff",
+     params: [{
+              name: "greet",
+              type: status.types.TEXT,
+              suggestions: applySuggestions
+             }]
+ });
 
 function suggestionsContainerStyle(suggestionsCount) {
     return {
@@ -40,34 +52,32 @@ var valueStyle = {
 };
 
 function helloSuggestions() {
-    var suggestions = ["Apply for policy", "Show my policies"].map(function(entry) {
-        return status.components.touchable(
-            {onPress: status.components.dispatch([status.events.SET_VALUE, entry])},
-            status.components.view(
-                suggestionsContainerStyle,
-                [status.components.view(
-                    suggestionSubContainerStyle,
-                    [
-                        status.components.text(
-                            {style: valueStyle},
-                            entry
-                        )
-                    ]
-                )]
-            )
-        );
-    });
-
-    // Let's wrap those two touchable buttons in a scrollView
-    var view = status.components.scrollView(
-        suggestionsContainerStyle(2),
-        suggestions
-    );
-
-    // Give back the whole thing inside an object.
-    return {markup: view};
+    return suggestions(["Apply for policy", "Show my policies"]);
 }
 
+function applySuggestions() {
+    return suggestions(["Please select your origin."]);
+}
+
+status.addListener("on-message-send", function (params, context) {
+    var result = {
+            err: null,
+            data: null,
+            messages: []
+        };
+
+    try {
+        if (params.message == "Apply for policy") {
+            result["text-message"] = "Please select your origin.";
+        } else {
+            result["text-message"] = "You're amazing, master!";
+        }
+    } catch (e) {
+        result.err = e;
+    }
+
+    return result;
+});
 
 /*status.command({
      name: "hello",
@@ -89,3 +99,36 @@ function helloSuggestions() {
          }
  });
 */
+
+
+/** Helpers */
+
+function suggestions(strings) {
+    var suggestions = strings.map(function(entry) {
+        return status.components.touchable(
+            {onPress: status.components.dispatch([status.events.SET_VALUE, entry])},
+            status.components.view(
+                suggestionsContainerStyle,
+                [status.components.view(
+                    suggestionSubContainerStyle,
+                    [
+                        status.components.text(
+                            {style: valueStyle},
+                            entry
+                        )
+                    ]
+                )]
+            )
+        );
+    });
+    return suggestionsInScrollView(suggestions)
+}
+
+function suggestionsInScrollView(suggestions) {
+    // Let's wrap those two touchable buttons in a scrollView
+    var view = status.components.scrollView(
+        suggestionsContainerStyle(2),
+        suggestions
+    );
+    return {markup: view}
+}
