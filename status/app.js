2ca4c21847d9fcb9d2b9411f5d07fe4d0264b759
sunfeld <sunfeld@sunis.nl>
2017-06-06 09:34:14 +0200

    Hello world status app

--- status/app.js
+++
@@ -1,6 +0,0 @@
-function onContentLoaded() {
-  var accountSpan = document.getElementById("account");
-  accountSpan.innerHTML = "Hello world";
-  //accountSpan.innerHTML = (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts);
-}
-document.addEventListener("DOMContentLoaded", onContentLoaded);
