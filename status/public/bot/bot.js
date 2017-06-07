0e3ac057f60dfe4c61b3ac3dadd7de8ce04c8e08
Will Saults <wsaults@yahoo.com>
2017-06-06 21:33:55 -0500

    Added chat-bot responses and demo prompts.

--- status/public/bot/bot.js
+++ status/public/bot/bot.js
@@ -1,6 +1,6 @@
 status.addListener("init",
  function (params, context) {
-    return {"text-message": "Hi, this is Lisa from Etherisc. How may I help you?"};
+    return {"text-message": "Hi, this is Lisa from Etherisc. How may I help you? \nDEMO: Apply for policy"};
 });
 
 status.command({
@@ -11,21 +11,143 @@ status.command({
      params: [{
               name: "greet",
               type: status.types.TEXT,
-              suggestions: helloSuggestions
+              suggestions: startSuggestions
              }]
  });
 
  status.command({
-     name: "apply",
-     title: "Apply for a policy",
-     description: "Options for applying",
+     name: "cancel",
+     title: "Cancel",
+     description: "Cancel the current operation",
      color: "#0000ff",
      params: [{
-              name: "greet",
+              name: "cancel",
               type: status.types.TEXT,
-              suggestions: applySuggestions
+              suggestions: cancelSuggestions
              }]
  });
+
+ status.command({
+     name: "back",
+     title: "Back",
+     description: "Takes you back",
+     color: "#0000ff",
+     params: [{
+              name: "back",
+              type: status.types.TEXT,
+              suggestions: backSuggestions
+             }]
+ });
+
+function startSuggestions() {
+    return suggestions(["Apply for policy", "Show my policies"]);
+}
+
+function cancelSuggestions() {
+    return suggestions(["Cancel"]);
+}
+
+function backSuggestions() {
+    return suggestions(["Go back"]);
+}
+
+var MESSAGE  = {
+    APPLY  : "Apply for policy",
+    YES  : "Yes",
+    CANCEL : "Cancel",
+    BACK   : "Go Back",
+    DESTINATION : "Barcelona",
+    DEPART_DATE : "Today",
+    FLIGHT : "14.50 KLM 1665",
+    PREMIUM : "0.16 ETH",
+    ACCEPT : "Accept"
+};
+
+status.addListener("on-message-send", function (params, context) {
+    var result = {
+            err: null,
+            data: null,
+            messages: []
+        };
+
+    try {
+        var message = "";
+        switch(params.message) {
+            case MESSAGE.APPLY: {
+                message = "I see that you are near Schiphol Airport. Do you intend to start there? \nDEMO: Yes";
+                break;
+            }
+            case MESSAGE.YES: {
+                message = "Please select your Destination. \nDEMO: Barcelona";
+                break;
+            }
+            case MESSAGE.DESTINATION: {
+                message = "When will the flight depart? \nDEMO: Today";
+                break;
+            }
+            case MESSAGE.DEPART_DATE: {
+                message = "I found some flights for you. Please select your flight. \nDEMO: 14.50 KLM 1665";
+                break;
+            }
+            case MESSAGE.FLIGHT: {
+                message = "You selected flight 14.50 KLM 1665 from Schiphol Airport to Barcelona today. Please enter the desired premium. \nDEMO: 0.16 ETH";
+                break;
+            }
+            case MESSAGE.PREMIUM: {
+                message = "Depending on the delay, you'll get the following payouts: \n15 - 29 min  1.58 ETH\n30 - 44 min  2.37 ETH\n      45+ min  4.75 ETH\n  Cancelled  7.89 ETH. \n\nYou can alter the premium or apply for the policy now. \nDEMO: Accept";
+                break;
+            }
+            case MESSAGE.ACCEPT: {
+                message = "Congrats! You have successfully applied for a FlightDelay Policy. The Tx Hash is 0xdeadbeef...";
+                break;
+            }
+            case MESSAGE.CANCEL:
+                message = "You canceled the transaction. Hope to see you again soon!";
+                break;
+            case MESSAGE.BACK:
+                message = "How can I help you? \nDEMO: Apply for policy";
+                break;
+            default: {
+                message = "Sorry, I'm not sure I understand.";
+            }
+        }
+    } catch (e) {
+        result.err = e;
+    }
+
+    result["text-message"] = message
+    return result;
+});
+
+
+/** Suggestion helpers */
+
+function suggestions(strings) {
+    var suggestions = strings.map(function(entry) {
+        return status.components.touchable(
+            {onPress: status.components.dispatch([status.events.SET_VALUE, entry])},
+            status.components.view(
+                suggestionsContainerStyle,
+                [status.components.view(
+                    suggestionSubContainerStyle,
+                    [
+                        status.components.text(
+                            {style: valueStyle},
+                            entry
+                        )
+                    ]
+                )]
+            )
+        );
+    });
+    return suggestionsInScrollView(suggestions)
+}
+
+function suggestionsInScrollView(suggestions) {
+    // Let's wrap those buttons in a scrollView
+    var view = status.components.scrollView(suggestionsContainerStyle(suggestions.count), suggestions);
+    return {markup: view}
+}
 
 function suggestionsContainerStyle(suggestionsCount) {
     return {
@@ -50,85 +172,3 @@ var valueStyle = {
     fontFamily: "font",
     color: "#000000de"
 };
-
-function helloSuggestions() {
-    return suggestions(["Apply for policy", "Show my policies"]);
-}
-
-function applySuggestions() {
-    return suggestions(["Please select your origin."]);
-}
-
-status.addListener("on-message-send", function (params, context) {
-    var result = {
-            err: null,
-            data: null,
-            messages: []
-        };
-
-    try {
-        if (params.message == "Apply for policy") {
-            result["text-message"] = "Please select your origin.";
-        } else {
-            result["text-message"] = "You're amazing, master!";
-        }
-    } catch (e) {
-        result.err = e;
-    }
-
-    return result;
-});
-
-/*status.command({
-     name: "hello",
-     title: "HelloBot",
-     description: "Helps you say hello",
-     color: "#CCCCCC",
-     preview: function (params) {
-             var text = status.components.text(
-                 {
-                     style: {
-                         marginTop: 5,
-                         marginHorizontal: 0,
-                         fontSize: 14,
-                         fontFamily: "font",
-                         color: "black"
-                     }
-                 }, "Hello from the other side!");
-             return {markup: status.components.view({}, [text])};
-         }
- });
-*/
-
-
-/** Helpers */
-
-function suggestions(strings) {
-    var suggestions = strings.map(function(entry) {
-        return status.components.touchable(
-            {onPress: status.components.dispatch([status.events.SET_VALUE, entry])},
-            status.components.view(
-                suggestionsContainerStyle,
-                [status.components.view(
-                    suggestionSubContainerStyle,
-                    [
-                        status.components.text(
-                            {style: valueStyle},
-                            entry
-                        )
-                    ]
-                )]
-            )
-        );
-    });
-    return suggestionsInScrollView(suggestions)
-}
-
-function suggestionsInScrollView(suggestions) {
-    // Let's wrap those two touchable buttons in a scrollView
-    var view = status.components.scrollView(
-        suggestionsContainerStyle(2),
-        suggestions
-    );
-    return {markup: view}
-}
