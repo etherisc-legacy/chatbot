2a3125b56e8050754b1404ec4aea3e9e9c31c38a
Freek <freekpst@gmail.com>
2017-06-08 20:38:54 +0200

    Added validation

--- token/src/bot.js
+++ token/src/bot.js
@@ -32,7 +32,7 @@ bot.onEvent = function(session, message) {
       break
     case 'Payment':
       console.log('Message type: PAYMENT')
-      onPayment(session, message)
+      welcome(session)
       break
     case 'PaymentRequest':
       console.log('Message type: PAYMENT REQUEST')
@@ -47,15 +47,15 @@ function onMessage(session, message) {
   switch(processStep) {
     case ProcessStepEnum.ORIGIN:
       console.log('PROCESS STEP: ORIGIN')
-      requestDestination(session, message)
+      validateOrigin(session, message)
       break;
     case ProcessStepEnum.DESTINATION:
       console.log('PROCESS STEP: DESTINATION')
-      requestDate(session, message)          
+      validateDestination(session, message)          
       break;
     case ProcessStepEnum.DATE:
       console.log('PROCESS STEP: DATE')
-      showFlights(session , message)
+      validateDate(session , message)
       break;
     case ProcessStepEnum.FLIGHT:
       console.log('PROCESS STEP: FLIGHT')
@@ -63,7 +63,7 @@ function onMessage(session, message) {
       break;
     case ProcessStepEnum.PREMIUM:
       console.log('PROCESS STEP: PREMIUM')
-      requestConfirmation(session, message)
+      validatePremium(session, message)      
       break;
     default:
       console.log('PROCESS STEP: DEFAULT')
@@ -73,18 +73,16 @@ function onMessage(session, message) {
 
 function onCommand(session, command) {
   console.log('COMMAND', command.content.value)
-  switch (command.content.value) {
+  const valueJSON = JSON.parse(command.content.value)
+  switch (valueJSON.command) {
     case 'applyPolicy':
       applyPolicy(session)
       break
     case 'showPolicies':
       showPolicies(session)
       break
-    case 'cancel':
-      cancel(session)
-      break
     case 'chooseFlight':
-      saveChosenFlight(session, command)
+      saveChosenFlight(session, valueJSON.extraInfo, command.content.body)
       requestPremium(session)
       break
     case 'acceptPremium':
@@ -93,32 +91,20 @@ function onCommand(session, command) {
     case 'changePremium':
       requestNewPremium(session)
       break
+    case 'cancel':
+      cancel(session)
+      break
+    case 'test':
+      test(session)
+      break
     }
-}
-
-function onPayment(session, message) {
-  if (message.fromAddress == session.config.paymentAddress) {
-    // handle payments sent by the bot
-    if (message.status == 'confirmed') {
-      // perform special action once the payment has been confirmed
-      // on the network
-    } else if (message.status == 'error') {
-      // oops, something went wrong with a payment we tried to send!
-    }
-  } else {
-    // handle payments sent to the bot
-    if (message.status == 'unconfirmed') {
-      // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
-    } else if (message.status == 'confirmed') {
-      // handle when the payment is actually confirmed!
-    } else if (message.status == 'error') {
-      sendMessage(session, `There was an error with your payment!🚫`);
-    }
-  }
 }
 
 // STATES
+
+function test(session) {
+  //TODO call API
+}
 
 function welcome(session) {
   sendMessageSimple(session, `Hi, this is Lisa from Etherisc.`)
@@ -128,46 +114,91 @@ function welcome(session) {
 function applyPolicy(session) {
   console.log('FUNCTION: applyPolicy')
   setProcessStep(session, ProcessStepEnum.ORIGIN)
-  sendMessageWithCancel(session, 'Please enter your origin.')
+  sendMessageWithCancel(session, 'Please enter your origin. (IATA-Code, e.g. JFK)')
+}
+
+function isValidAirportCode(airportCode) {
+  //Regex to validate whether airport code format (ABZ) is correct  
+  return /^[A-Z]{3}$/.test(airportCode)
+}
+
+function validateOrigin(session, message) {
+  console.log('MESSAGE', message)
+  const hasCorrectFormat = isValidAirportCode(message.body)
+  if (hasCorrectFormat) {
+    requestDestination(session, message.body)
+  } else {
+    sendMessageWithCancel(session, 'Format incorrect. Please enter the IATA-code (e.g. JFK) of your origin')
+  }
 }
 
 function requestDestination(session, message) {
-  session.set('Origin', message.content.body)
+  session.set('Origin', message)
   setProcessStep(session, ProcessStepEnum.DESTINATION)
-  sendMessageWithCancel(session, 'Please enter your destination.')
+  sendMessageWithCancel(session, 'Please enter your destination. (IATA-Code, e.g. JFK)')
+}
+
+function validateDestination(session, message) {
+  console.log('MESSAGE', message)
+  const hasCorrectFormat = isValidAirportCode(message.body)
+  if (hasCorrectFormat) {
+    requestDate(session, message.body)
+  } else {
+    sendMessageWithCancel(session, 'Format incorrect. Please enter the IATA-code (e.g. JFK) of your destination')  
+  }
 }
 
 function requestDate(session, message) {
-  session.set('Destination', message.content.body)
+  session.set('Destination', message)
   setProcessStep(session, ProcessStepEnum.DATE)
-  sendMessageWithCancel(session, 'Please enter the flight date in a DD/MM/YY format')
-  //TODO ask for date
+  sendMessageWithCancel(session, 'Please enter the flight date in a YYYY-MM-DD format')
+}
+
+function validateDate(session, message) {
+  const dateInMilliseconds = Date.parse(message.body)
+  console.log('DATE IN MILLISECONDS', dateInMilliseconds)
+  if(isNaN(dateInMilliseconds) || (typeof dateInMilliseconds !== 'number')) {
+    sendMessageWithCancel(session, 'Invalid date. Please enter the flight date in a YYYY-MM-DD format')
+  } else if (dateInMilliseconds - Date.now() <= (24*60*60*1000)) {
+    sendMessageSimple(session, 'Cannot insure flights with a departure time of within 24 hours.')
+    sendMessageWithCancel(session, 'Please enter the flight date in a YYYY-MM-DD format')
+  } else {
+    showFlights(session, message.body)
+  }
 }
 
 function showFlights(session, message) {
-  session.set('Date', message.content.body)
+  session.set('Date', message)
   setProcessStep(session, ProcessStepEnum.FLIGHT)
   sendMessageFlights(session, 'Please select your flight')
-  //TODO show flights
+  //TODO DO AN API CALL
 }
 
-function saveChosenFlight(session, command) {
-  //console.log('COMMAND DATA', command)
-  const flightId = command.content.body
-  console.log('FLIGHT ID', flightId)
-  session.set('Flight', flightId)
+function saveChosenFlight(session, flightId, flightInfo) {
+  console.log('SAVE CHOSEN FLIGHT', flightId, flightInfo)
+  session.set('FlightId', flightId)
+  session.set('FlightInfo', flightInfo)
 }
 
 function requestPremium(session) {
   setProcessStep(session, ProcessStepEnum.PREMIUM)
   const origin = session.get('Origin');
   const destination = session.get('Destination')
-  const flightId = session.get('Flight');
+  const flightInfo = session.get('FlightInfo');
   const date = session.get('Date');
-  console.log('Origin', origin, 'Destination', destination, 'flightId', flightId, 'date', date)
-  const message = `You selected flight ${flightId} from ${origin} to ${destination} on ${date}.
-                    Please enter the desired premium`
+  console.log('Origin', origin, 'Destination', destination, 'flightInfo', flightInfo, 'date', date)
+  const message = `You selected flight ${flightInfo} from ${origin} to ${destination} on ${date}.
+  Please enter the desired premium in ETH (e.g. 0.5 or 2)` 
   sendMessageWithCancel(session, message)
+}
+
+function validatePremium(session, message) {
+  const isNumber = !isNaN(parseFloat(message.body)) && isFinite(message.body);
+  if(isNumber && message.body > 0) {
+    requestConfirmation(session, message.body)
+  } else {
+    sendMessageWithCancel(session, 'Invalid premium. Please enter a premium in ETH')
+  }
 }
 
 function requestConfirmation(session, message) {
@@ -199,7 +230,8 @@ function showPolicies(session) {
 
 function cancel(session) {
   //TODO Clear session variables
-  clearSession();
+  console.log('CANCEL called')
+  clearSession(session);
   sendMessageAction(session, `You canceled the apply process.`)
 }
 
@@ -229,8 +261,9 @@ function sendMessageSimple(session, message) {
 
 function sendMessageAction(session, message) {
   let controls = [
-    {type: 'button', label: 'Apply for policy', value: 'applyPolicy'},
-    {type: 'button', label: 'Show my policies', value: 'showPolicies'},
+    {type: 'button', label: 'Apply for policy', value: createValueComposite('applyPolicy', '')},
+    {type: 'button', label: 'Show my policies', value: createValueComposite('showPolicies', '')},
+    {type: 'button', label: 'Test', value: createValueComposite('test', 'flowers')},
   ]
   session.reply(SOFA.Message({
     body: message,
@@ -241,7 +274,7 @@ function sendMessageAction(session, message) {
 
 function sendMessageWithCancel(session, message) {
   let controls = [
-    {type: 'button', label: 'Cancel', value: 'cancel'},
+    {type: 'button', label: 'Cancel', value: createValueComposite('cancel', '')},
   ]
   session.reply(SOFA.Message({
     body: message,
@@ -250,13 +283,15 @@ function sendMessageWithCancel(session, message) {
   }))
 }
 
+//TODO Create this dynamically based on the flights we get from the API
 function sendMessageFlights(session, message) {
   let controls = [
     {type: 'group', label: 'View flights', controls: [
-      {type: 'button', label: '09:30 - Vueling VY4567', value: 'chooseFlight'},
-      {type: 'button', label: '12:30 - Transavia TK3439', value: 'chooseFlight'},
-      {type: 'button', label: '15:00 - KLM KM9980', value: 'chooseFlight'}
+      {type: 'button', label: '09:30 - Vueling VY4567', value: createValueComposite('chooseFlight', 'VY4567')},
+      {type: 'button', label: '12:30 - Transavia TK3439', value: createValueComposite('chooseFlight', 'TK3439')},
+      {type: 'button', label: '15:00 - KLM KM9980', value: createValueComposite('chooseFlight', 'KM9980')}
     ]},
+    {type: 'button', label: 'Cancel', value: createValueComposite('cancel', '')},
   ]
   session.reply(SOFA.Message({
     body: message,
@@ -267,8 +302,8 @@ function sendMessageFlights(session, message) {
 
 function sendMessagePremiumConfirm(session, message) {
   let controls = [
-    {type: 'button', label: 'Accept', value: 'acceptPremium'},
-    {type: 'button', label: 'Change premium', value: 'changePremium'},
+    {type: 'button', label: 'Accept', value: createValueComposite('acceptPremium', '')},
+    {type: 'button', label: 'Change premium', value: createValueComposite('changePremium', '')},
   ]
   session.reply(SOFA.Message({
     body: message,
@@ -277,15 +312,6 @@ function sendMessagePremiumConfirm(session, message) {
   }))
 }
 
-function sendMessage(session, message) {
-  let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
-  ]
-  session.reply(SOFA.Message({
-    body: message,
-    controls: controls,
-    showKeyboard: false,
-  }))
+function createValueComposite(value, extraInfo) {
+  return `{"command": "${value}", "extraInfo": "${extraInfo}"}`
 }
